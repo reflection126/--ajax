b7fece7038387f82a9c45100435d4a2e106d94e0
reflection126 <reflection126@sina.com>
2018-01-13 09:21:25 +0800

    链接数据库的封装

--- insertDb.php
+++ insertDb.php
@@ -17,21 +17,7 @@
 
     <?php
 
-    define('DB_HOST', '127.0.0.1');  //地址
-    define('DB_USER', 'root');       //用户名
-    define('DB_PWD', '');            //密码
-    define('DB_NAME', 'register');    //数据库名
-
-    //mysqli_connect和mysqli_close 配对使用
-    //mysql_connect 和 mysql_close配对使用
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
-    if (!$conn) {
-        die('Could not connect: ' . mysqli_error($conn));
-    }
-    //echo '数据库连接成功！';
-
-    //设置字符编码
-    mysqli_query($conn, "set names utf8");
+    include("connectToDb.php");
 
     //判断用户名或密码是否为空，并加密密码
     if(trim($_POST['username'])!= "" and trim($_POST['password'])!= ""){
