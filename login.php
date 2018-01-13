9ba51916aa9598db515c7a707dcc1ee7cb7f0976
reflection126 <reflection126@sina.com>
2018-01-13 09:33:30 +0800

    删除login.php连接数据库部分，引入connectDb.php

--- login.php
+++ login.php
@@ -10,27 +10,9 @@ if (isset($_GET['action']) == 'login') {
     //echo $clean['username'];
     //echo $clean['password'];
 
-    define('DB_HOST', '127.0.0.1');  //地址
-    define('DB_USER', 'root');       //用户名
-    define('DB_PWD', '');            //密码
-    define('DB_NAME', 'register');    //数据库名
-
-    //mysqli_connect和mysqli_close 配对使用
-    //mysql_connect 和 mysql_close配对使用
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
-    if (!$conn) {
-        die('Could not connect: ' . mysqli_error($conn));
-    }
-    //echo '数据库连接成功！';
-
-    //设置字符编码
-    mysqli_query($conn, "set names utf8");
-    //echo $clean['username'];
+   include ("connectToDb.php");
     // //查询
     $sql = "select * from tb_user where username = '{$clean['username']}'";
-    //尝试输出查询语句：
-    //echo $sql;
-
     $queryResult = mysqli_query($conn, $sql);
     if (!$queryResult) {
         die('could not query' . mysqli_error($conn));
@@ -62,7 +44,7 @@ if (isset($_GET['action']) == 'login') {
 
 
 }
-//include 'infoEdit.php';
+
 ?>
 <!DOCTYPE html>
 
